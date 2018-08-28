2c79bc119c0a39f66c0979c6e40bf884934e2068
girishKM <35058644+girishKM@users.noreply.github.com>
2018-08-28 18:41:41 +0530

    added GITKAT

---
+++ GITKat/Object Repository/12cTest.rs
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>12cTest</name>
+   <tag></tag>
+   <elementGuidId>92abe1ad-df99-45a8-8d9c-5526c19e3635</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <restRequestMethod></restRequestMethod>
+   <restUrl></restUrl>
+   <serviceType>SOAP</serviceType>
+   <soapBody>&lt;soapenv:Envelope xmlns:soapenv=&quot;http://schemas.xmlsoap.org/soap/envelope/&quot; xmlns:bpel=&quot;http://xmlns.oracle.com/CIapplication/CIproject/BPELProcess2&quot;>
+   &lt;soapenv:Header/>
+   &lt;soapenv:Body>
+      &lt;bpel:process>
+         &lt;bpel:input>samsung&lt;/bpel:input>
+      &lt;/bpel:process>
+   &lt;/soapenv:Body>
+&lt;/soapenv:Envelope></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod>SOAP</soapRequestMethod>
+   <soapServiceFunction>process</soapServiceFunction>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+</verificationScript>
+   <wsdlAddress>http://172.30.64.37:7101/soa-infra/services/default/CIproject/bpelprocess2_client_ep?WSDL</wsdlAddress>
+</WebServiceRequestEntity>
---
+++ GITKat/Test Suites/tsA.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>tsA</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-08-28T18:00:09</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d5494786-f085-49fc-94df-7a47024274e4</testSuiteGuid>
+   <testCaseLink>
+      <guid>18fe5391-f744-4801-a27e-6452eca1d27b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ts1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
