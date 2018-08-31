393b097828185b816ad4352f84b0e2cf3261abd1
girishKM <08011992+girishKM@users.noreply.github.com>
2018-08-31 15:50:46 +0530

    added ts

--- GITKat/Test Suites/tsA.ts
+++ GITKat/Test Suites/tsA.ts
@@ -11,4 +11,10 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>d5494786-f085-49fc-94df-7a47024274e4</testSuiteGuid>
+   <testCaseLink>
+      <guid>8112ce3e-0b68-4553-84d7-5d952de60db4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ts1</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
